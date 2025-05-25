a7a94468ae2db429a9211ddfdaa748052e6ddfc8
Bartłomiej Stefański <bartex00001@gmail.com>
2025-05-25 19:07:09 +0200

    Created VFS directory and runnable target

--- src/example_sbi/entry.c
+++ src/example_sbi/entry.c
@@ -1,10 +1,15 @@
 #include <stdbigos/sbi.h>
 #include <stdbigos/types.h>
 
+#include "../kernel/vfs/vfs.h"
+
 static void sbi_puts(const char* str) {
 	while (*str) sbi_debug_console_write_byte(*str++);
 }
 
 void main([[maybe_unused]] u32 hartid, [[maybe_unused]] const void* fdt) {
 	sbi_puts("Hello world\n");
+
+	// DEBUG: just for testing CMakes
+	vfsmain();
 }
