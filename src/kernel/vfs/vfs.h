9a1e7b7ee77bb303e2ece743037e2f25b45f9e9c
Igor Hanczaruk <igor.hanczaruk@gmail.com>
2025-05-27 01:50:53 +0000

    Basic mount tree structure with functionality of adding new mountpoints

--- src/kernel/vfs/vfs.h
+++ src/kernel/vfs/vfs.h
@@ -1,7 +1,17 @@
 #ifndef _VFS_
 #define _VFS_
 
+#include <stdbigos/types.h>
+
 // DEBUG for testing CMake configuration
 void vfsmain();
+
+#define SERVICE_HANDLE void* //placeholder for now
+
+typedef struct VFS_PATH
+{
+    u16 size;
+    char** path;
+} VFS_PATH;
 
 #endif
