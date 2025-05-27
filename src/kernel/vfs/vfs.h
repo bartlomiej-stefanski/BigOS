4bc0024893714f478ed837eac7a0a819c573b64a
Maksymilian Gojło <347809@uwr.edu.pl>
2025-05-27 08:51:04 +0200

    vfs: format

--- src/kernel/vfs/vfs.h
+++ src/kernel/vfs/vfs.h
@@ -6,12 +6,11 @@
 // DEBUG for testing CMake configuration
 void vfsmain();
 
-#define SERVICE_HANDLE void* //placeholder for now
+#define SERVICE_HANDLE void* // placeholder for now
 
-typedef struct VFS_PATH
-{
-    u16 size;
-    char** path;
+typedef struct VFS_PATH {
+	u16 size;
+	char** path;
 } VFS_PATH;
 
 #endif
