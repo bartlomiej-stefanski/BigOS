0df2e8a43876277575120769d317fae80cf2761b
Bartłomiej Stefański <bartex00001@gmail.com>
2025-06-08 14:13:13 +0200

    Added new error types for Mount Tree

--- src/lib/stdbigos/error.c
+++ src/lib/stdbigos/error.c
@@ -6,6 +6,12 @@ const char* get_error_msg(error_t err) {
 	switch (err) {
 	case ERR_NONE:             return "";
 	case ERR_INVALID_ARGUMENT: return "Invalid argument";
-	default:                   return "Error message was not provided";
+
+	case ERR_MALLOC_FAILED: return "Malloc failed";
+
+	case ERR_MT_MOUNTPOINT_EXSITS: return "Mount point already exists";
+	case ERR_MT_UNKNOWN:           return "Unknown Mount Tree error";
+
+	default: return "Error message was not provided";
 	}
 }
