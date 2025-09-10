64c206a6784ea03ed3efa9c41639ae3b1398f247
Bartlomiej Stefanski <bartex00001@gmail.com>
2025-09-10 22:38:52 +0200

    Add pstring read/write methods for basic types

--- include/stdbigos/pstring.h
+++ include/stdbigos/pstring.h
@@ -1,7 +1,10 @@
 #ifndef STDBIGOS_PSTRING_H
 #define STDBIGOS_PSTRING_H
 
+#include <stdbigos/error.h>
 #include <stdbigos/types.h>
+#include <stddef.h>
+
 
 // Some of the following functions that are string.h lookalikes are
 // more-or-less analogous to their [xxx]n[yyy] counterparts but check
@@ -53,5 +56,47 @@ size_or_err_t pstring_cat(pstring_t* src1, size_t from, const pstring_t* src2);
 /// Returns the number of bytes written into dest.
 [[nodiscard]]
 size_or_err_t pstring_concat(pstring_t* dest, const pstring_t* first, const pstring_t* second);
+
+#define PSTRING_WRITE(type)                                                          \
+	size_or_err_t pstring_write_##type(pstring_t* dest, size_t offset, type value) { \
+		if (dest == NULL) {                                                          \
+			return (size_or_err_t){.err = ERR_INVALID_ARGUMENT};                     \
+		}                                                                            \
+		if (offset + sizeof(type) > dest->len) {                                     \
+			return (size_or_err_t){.err = ERR_BUFFER_TOO_SHORT};                     \
+		}                                                                            \
+		*(type*)(dest->data + offset) = value;                                       \
+		return (size_or_err_t){.val = sizeof(type)};                                 \
+	}
+
+[[nodiscard]] inline PSTRING_WRITE(u8)
+[[nodiscard]] inline PSTRING_WRITE(u16)
+[[nodiscard]] inline PSTRING_WRITE(u32)
+[[nodiscard]] inline PSTRING_WRITE(u64)
+[[nodiscard]] inline PSTRING_WRITE(i8)
+[[nodiscard]] inline PSTRING_WRITE(i16)
+[[nodiscard]] inline PSTRING_WRITE(i32)
+[[nodiscard]] inline PSTRING_WRITE(i64)
+
+#define PSTRING_READ(type)                                                                    \
+	size_or_err_t pstring_read_##type(const pstring_t* src, size_t offset, type* out_value) { \
+		if (src == NULL || out_value == NULL) {                                               \
+			return (size_or_err_t){.err = ERR_INVALID_ARGUMENT};                              \
+		}                                                                                     \
+		if (offset + sizeof(type) > src->len) {                                               \
+			return (size_or_err_t){.err = ERR_BUFFER_TOO_SHORT};                              \
+		}                                                                                     \
+		*out_value = *(type*)(src->data + offset);                                            \
+		return (size_or_err_t){.val = sizeof(type)};                                          \
+	}
+
+[[nodiscard]] inline PSTRING_READ(u8)
+[[nodiscard]] inline PSTRING_READ(u16)
+[[nodiscard]] inline PSTRING_READ(u32)
+[[nodiscard]] inline PSTRING_READ(u64)
+[[nodiscard]] inline PSTRING_READ(i8)
+[[nodiscard]] inline PSTRING_READ(i16)
+[[nodiscard]] inline PSTRING_READ(i32)
+[[nodiscard]] inline PSTRING_READ(i64)
 
 #endif
